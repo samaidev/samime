98c6c118491a1ce297dcc81d54fec2c4681cb139
samaidev <samaidev@users.noreply.github.com>
2026-07-05 10:53:18 +0000

    Fix TSF header: add winsock2.h + tchar.h includes for SOCKET/_T types

--- internal/winime/cpp/samime_tsf.h
+++ internal/winime/cpp/samime_tsf.h
@@ -15,7 +15,12 @@
 #pragma once
 
 #define WIN32_LEAN_AND_MEAN
+#define _UNICODE
+#define UNICODE
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #include <windows.h>
+#include <tchar.h>
 #include <ole2.h>
 #include <olectl.h>
 #include <msctf.h>
